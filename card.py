ed48834e3d08f15e0f4ca804a01f4764f2dbf136
Borschevskyi <Borsch2556@gmail.com>
2023-06-24 16:40:28 +0300

    Edit

--- card.py
+++ card.py
@@ -2,19 +2,19 @@ class Card:
     ALLOWED_STATUSES = ["new", "active", "blocked"]
 
     def __init__(
-            self,
-            pan: str,
-            expiration_date: str,
-            cvv: str,
-            issue_date: str,
-            user_id: str,
-            status: ALLOWED_STATUSES,
+        self,
+        pan: str,
+        expiration_date: str,
+        cvv: str,
+        issue_date: str,
+        owner_id: str,
+        status: ALLOWED_STATUSES,
     ) -> None:
         self.pan = pan
         self.expiration_date = expiration_date
         self.cvv = cvv
         self.issue_date = issue_date
-        self.user_id = user_id
+        self.owner_id = owner_id
         self.status = status
 
     def activate(self):
